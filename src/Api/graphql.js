ed8e7669259ada041f83e2ed314564f305871af8
Jose Elias <jbarraza@mangus.org>
2021-12-15 00:18:12 -0500

    add config

--- src/Api/graphql.js
+++ src/Api/graphql.js
@@ -7,7 +7,7 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = "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";
+  const token = "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";
   return {
     headers: {
       ...headers,
